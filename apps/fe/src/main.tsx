a6bc51fe5e60379e5dcb56c888b9bd3399b35252
bhupesh <bhupeshverma29bv@gmail.com>
2026-09-01 02:38:40 +0530

    fix: @types/node in fe

--- apps/fe/src/main.tsx
+++ apps/fe/src/main.tsx
@@ -4,15 +4,18 @@ import "./index.css";
 import App from "./App";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { AuthProvider } from "./context/AuthContext";
+import { StrictMode } from "react";
 
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
-  <QueryClientProvider client={queryClient}>
-    <AuthProvider>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </AuthProvider>
-  </QueryClientProvider>,
+  <StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <AuthProvider>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </AuthProvider>
+    </QueryClientProvider>
+  </StrictMode>,
 );
